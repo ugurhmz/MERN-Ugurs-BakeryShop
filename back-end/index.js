f1e4486c756e785ad7a7c35c5c07d4cb63b07d6c
ugurhmz <craxx67@gmail.com>
2024-01-09 17:07:48 +0300

    DB connection success MongoDB

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,27 @@
 import express from 'express'
+import dotenv from 'dotenv'
+dotenv.config()
+import connectDB from './database/db.js'
+import cors from 'cors'
+
+// MongoDB
+connectDB()
+
 const app = express()
+app.use(cors({
+  origin: true,
+  credentials: true,
+}));
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+
+
 
 app.get("/", (req,res) => {
     res.send("Hello World . . .")
 })
 
-app.listen(3500, () => {
-    console.log("Listenin 3500 . . .")
-})
+app.listen(process.env.PORT || 7500, () => {
+    console.log(`Listening: ${process.env.PORT}`)
+  })
